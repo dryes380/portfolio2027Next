7e921e10f33bd2a5f3a3fdcef68418fc0cd7ebea
Vincent Kouoï <vincentkouoi@gmail.com>
2023-03-16 15:57:58 +0100

    footer set up

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { SocialIcon } from "react-social-icons";
 import "./Footer.css";
 
 const Footer = () => {
+  const [seconds, setSeconds] = useState(new Date().getSeconds());
+
+  const GetTime = () => {
+    setSeconds(new Date().getSeconds());
+    console.count(seconds);
+  };
+
+  setTimeout(() => {
+    GetTime();
+  }, 1000);
+
+  const str = new Date().toLocaleString("fr-FR", { timeZone: "UTC" });
   return (
     <footer className="footerContainer">
       <div className="footerContent">
-        <div>Date et Heure</div>
-        <div>Droits d'auteurs Vincent Kouoï 2023</div>
-        <div>Réseaux sociaux</div>
+        <div className="footerTime">{str}</div>
+        <div className="footerRights">Droits d'auteurs Vincent Kouoï 2023</div>
+        <div className="footerSocialIcons">
+          <a href="https://www.linkedin.com/in/vincent-kouoi-5079b1b1/">
+            <SocialIcon
+              style={{ right: 6, width: 40, height: 40 }}
+              network="linkedin"
+            />
+          </a>
+          <a href="https://github.com/xp380">
+            <SocialIcon network="github" style={{ width: 40, height: 40 }} />
+          </a>
+        </div>
       </div>
     </footer>
   );
