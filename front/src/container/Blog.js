3088a27add772717b4c454543b06c0c8b8a1748e
Vincent Kouoï <vincentkouoi@gmail.com>
2023-03-20 16:23:01 +0100

    first step for blog

--- front/src/container/Blog.js
+++ front/src/container/Blog.js
@@ -1,10 +1,43 @@
 import React from "react";
+import { Articles } from "../data/Articles";
 
 const Blog = () => {
   return (
-    <>
-      <div className="Content">Test</div>
-    </>
+    <div className="Content">
+      <h1 style={{ textAlign: "center" }}> Articles</h1>
+      {Articles.map((data) => {
+        return (
+          <div style={{ display: "flex", gap: 10 }}>
+            <div
+              style={{
+                width: "100%",
+                border: "2px solid black",
+                height: 300,
+                padding: 10,
+                margin: 10,
+              }}
+            >
+              <div style={{ display: "flex", gap: "40%" }}>
+                <div>{data.release}</div>
+                <div>{data.title}</div>
+                <div>{data.author}</div>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  margin: 10,
+                  position: "relative",
+                  right: 5,
+                }}
+              >
+                <div style={{ width: "20%", height: 200 }}>photo</div>
+                <div style={{ width: "80%" }}>{data.content}</div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 export default Blog;
---
+++ front/src/data/Articles.js
@@ -0,0 +1,18 @@
+export const Articles = [
+  {
+    id: 1,
+    author: "Vincent Kouoï",
+    title: "Example",
+    release: "15/02/2023",
+    content:
+      "So, you’ve been a web developer for a while, or maybe you’re jus a fast learner. Either way, hats off to you! Some of the beginner blogs outlined above may be a bit too junior for you               which is where Scotch.io comes in handy. This blog is full of               tutorials on everything from JavaScript to PHP. They also offer               some interesting courses that require minimal commitment and ca help take your coding expertise to the next level.",
+  },
+  {
+    id: 2,
+    author: "Eric Bompard",
+    title: "Demo",
+    release: "22/04/2023",
+    content:
+      "So, you’ve been a web developer for a while, or maybe you’re jus a fast learner. Either way, hats off to you! Some of the beginner blogs outlined above may be a bit too junior for you               which is where Scotch.io comes in handy. This blog is full of               tutorials on everything from JavaScript to PHP. They also offer               some interesting courses that require minimal commitment and ca help take your coding expertise to the next level.",
+  },
+];
