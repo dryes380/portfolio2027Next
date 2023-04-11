7d18bfd658cc0e33a550fe09c8cfa493cdc8bc00
Vincent Kouoï <vincentkouoi@gmail.com>
2023-04-11 15:20:22 +0200

    portfolio wip

--- front/src/container/Portfolio.js
+++ front/src/container/Portfolio.js
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, Divider, Modal } from 'antd'
+import { PortFolioData } from '../data/PortfolioData'
+import { GithubOutlined } from '@ant-design/icons';
 
 const Portfolio = () => {
+  const { Meta } = Card
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className="Content">Portfolio</div>
+    <div className="Content">
+      <h1 style={{ textAlign: "center" }}>Portfolio</h1>
+      <Divider />
+      <div className='blogArticles'>
+        {PortFolioData.map((data => {
+          return (
+            <Card
+              style={{
+                width: 300,
+                height: 340
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src={data.picture}
+                />
+              }
+              actions={[
+                <GithubOutlined key='source' onClick={showModal} />,
+              ]}
+            >
+              <Meta
+                title={data.title}
+                description={data.description}
+              />
+              <Modal title={data.stack} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+              </Modal>
+            </Card>
+          )
+        }))}
+      </div>
+    </div>
   );
 };
 
---
+++ front/src/data/PortfolioData.js
@@ -0,0 +1,34 @@
+export const PortFolioData = [
+	{
+		id: 1,
+		title: 'SuperQuizz',
+		description: 'quizz to have fun',
+		stack: 'React.js',
+		picture: 'https://media.istockphoto.com/id/1272478640/vector/retro-light-text-quiz-time-retro-light-bulb-vector-stock-illustration.jpg?s=612x612&w=0&k=20&c=ZCiSSDczdpCRGZcMzTNzStJYy8wwHomb39D0HFVjVb0=',
+		linkRedirect: 'https://superquizz.netlify.app/',
+		codeSource: '',
+		date: '',
+
+	},
+	{
+		id: 2,
+		title: 'APIBook',
+		description: 'web site for movies, manga and games',
+		stack: 'React.js, Context API',
+		picture: 'https://images.unsplash.com/photo-1589998059171-988d887df646?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&w=1000&q=80',
+		linkRedirect: '',
+		codeSource: '',
+		date: '',
+	},
+	{
+		id: 3,
+		title: 'Recipes food',
+		description: 'Web site to search your favorite food',
+		stack: 'React.js',
+		picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJcM50pbjm2jERNPv_wH4FBHpAH55FEkYVpj2wJeP-SNnfS565mVxQ8odh0uhgwSIHlqs&usqp=CAU',
+		linkRedirect: '',
+		codeSource: '',
+		date: ''
+	},
+
+];
