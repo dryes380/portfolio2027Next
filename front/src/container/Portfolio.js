6742a698c45822c2f1e25ec18bd0ed369995d7b5
Vincent Kouoï <vincentkouoi@gmail.com>
2023-05-11 14:44:38 +0200

    cleaned code

--- front/src/container/Portfolio.js
+++ front/src/container/Portfolio.js
@@ -1,19 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Card, Divider, Modal, Tooltip } from "antd";
-import { GithubOutlined, QuestionCircleOutlined } from "@ant-design/icons";
+import { Tabs } from "antd";
 
 import { PortFolioData } from "../data/PortfolioData";
+import Projects from "./Projects";
+import FavoriteList from "./FavoriteList";
 import "./Portfolio.css";
 
 const Portfolio = () => {
-  const { Meta } = Card;
   const [favorites, setFavorites] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [contentTitle, setContentTitle] = useState("");
-  const [contentStack, setContentStack] = useState("");
-  const [contentDate, setContentDate] = useState("");
-  const [contentPicture, setContentPicture] = useState("");
-
   useEffect(() => {
     setFavorites(PortFolioData);
   }, []);
@@ -25,76 +19,27 @@ const Portfolio = () => {
 
     setFavorites(newFavorites);
   };
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
+  const items = [
+    {
+      key: "1",
+      label: `List Project`,
+      children: (
+        <Projects favorites={favorites} handleFavorites={handleFavorites} />
+      ),
+    },
+    {
+      key: "2",
+      label: `Favorite Project`,
+      children: (
+        <FavoriteList favorites={favorites} handleFavorites={handleFavorites} />
+      ),
+    },
+  ];
 
   return (
     <div className="Content">
       <h1 className="portfolioGeneral">Portfolio</h1>
-      <Divider />
-      <div className="portfolioProjects">
-        {favorites.map((data) => {
-          return (
-            <Card
-              className="portfolioCards"
-              cover={
-                <img
-                  className="portfolioPictures"
-                  alt="example"
-                  src={data.picture}
-                  onClick={() => window.open(data.linkRedirect, "redirect")}
-                />
-              }
-              actions={[
-                <Tooltip title="Link to source code">
-                  <GithubOutlined
-                    key="source"
-                    onClick={() => window.open(data.codeSource, "source")}
-                  />
-                </Tooltip>,
-                <Tooltip title="Details project">
-                  <QuestionCircleOutlined
-                    key="details"
-                    onClick={() => {
-                      setIsModalOpen(true);
-                      setContentTitle(data.title);
-                      setContentStack(data.stack);
-                      setContentDate(data.date);
-                      setContentPicture(data.picture);
-                    }}
-                  />
-                </Tooltip>,
-                <button
-                  onClick={() => {
-                    console.log("test", data.id);
-                    handleFavorites(data.id);
-                  }}
-                >
-                  {data.favorite === true ? "Remove" : "Add"}
-                </button>,
-              ]}
-            >
-              <Meta title={data.title} description={data.description} />
-              <Modal
-                footer={null}
-                title={contentTitle}
-                open={isModalOpen}
-                centered
-                onCancel={handleCancel}
-              >
-                <div className="portfolioModal">
-                  <div className="modalDetails">
-                    <div>Stack:{contentStack}</div>
-                    <div>Date Création:{contentDate}</div>
-                  </div>
-                  <img className="modalPicture" alt="" src={contentPicture} />
-                </div>
-              </Modal>
-            </Card>
-          );
-        })}
-      </div>
+      <Tabs defaultActiveKey="1" items={items} />
     </div>
   );
 };
---
+++ front/src/container/Projects.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Card, Modal, Tooltip } from "antd";
+import {
+  GithubOutlined,
+  QuestionCircleOutlined,
+  StarOutlined,
+} from "@ant-design/icons";
+
+import "./Projects.css";
+
+const Projects = ({ favorites, handleFavorites }) => {
+  const { Meta } = Card;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [contentTitle, setContentTitle] = useState("");
+  const [contentStack, setContentStack] = useState("");
+  const [contentDate, setContentDate] = useState("");
+  const [contentPicture, setContentPicture] = useState("");
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <div className="portfolioProjects">
+      {favorites.map((data) => {
+        return (
+          <Card
+            className="portfolioCards"
+            cover={
+              <img
+                className="portfolioPictures"
+                alt="example"
+                src={data.picture}
+                onClick={() => window.open(data.linkRedirect, "redirect")}
+              />
+            }
+            actions={[
+              <Tooltip title="Link to source code">
+                <GithubOutlined
+                  key="source"
+                  onClick={() => window.open(data.codeSource, "source")}
+                />
+              </Tooltip>,
+              <Tooltip title="Details project">
+                <QuestionCircleOutlined
+                  key="details"
+                  onClick={() => {
+                    setIsModalOpen(true);
+                    setContentTitle(data.title);
+                    setContentStack(data.stack);
+                    setContentDate(data.date);
+                    setContentPicture(data.picture);
+                  }}
+                />
+              </Tooltip>,
+              <div
+                onClick={() => {
+                  handleFavorites(data.id);
+                }}
+              >
+                {data.favorite === true ? (
+                  <StarOutlined style={{ color: "yellow" }} />
+                ) : (
+                  <StarOutlined style={{ color: "black" }} />
+                )}
+              </div>,
+            ]}
+          >
+            <Meta title={data.title} description={data.description} />
+            <Modal
+              footer={null}
+              title={contentTitle}
+              open={isModalOpen}
+              centered
+              onCancel={handleCancel}
+            >
+              <div className="portfolioModal">
+                <div className="modalDetails">
+                  <div>Stack:{contentStack}</div>
+                  <div>Date Création:{contentDate}</div>
+                </div>
+                <img className="modalPicture" alt="" src={contentPicture} />
+              </div>
+            </Modal>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Projects;
